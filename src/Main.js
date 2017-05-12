ac21a87b0c86cf0a69195df98c3741a08d669986
Leandro Pinheiro <leosantana.music@gmail.com>
2017-05-12 00:13:57 -0300

    insert assets

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,16 @@
-class Main
+class Main extends Phaser.Game
 {
-	
+	constructor()
+	{
+		super(768, 450, Phaser.AUTO);
+
+		this.state.add('PreLoader', new Preloader);
+		this.state.start('PreLoader');
+	}
+
+	create()
+	{
+		console.log('sd');
+	}
 }
+new Main();
