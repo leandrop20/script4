caf4a32071b490b571a7e32f2561e925b083e078
Leandro Pinheiro <leosantana.music@gmail.com>
2017-05-26 00:33:15 -0300

    dev

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,15 @@
-class Root extends Container
+class Root extends Sprite
 {
 	constructor()
 	{
 		super();
 
-		var t = new Sprite('imgExample');
+		var t = new ImageSuper('imgExample');
 		t.position.set(130, 110);
 		t.scale.set(0.37);
 		this.addChild(t);
 
-		var t2 = new Sprite('imgLoad');
+		var t2 = new ImageSuper('imgLoad');
 		t2.position.set(Script4.width-90, 40);
 		this.addChild(t2);
 
@@ -17,7 +17,7 @@ class Root extends Container
 		spine.play('walk', true);
 		this.addChild(spine);
 
-		var bt = new SimpleButton('btExample', 100, 350);
+		var bt = new ButtonSuper('btExample', 100, 350);
 		this.addChild(bt);
 		bt.addEventListener(Event.TRIGGERED, onTest);
 		function onTest()
@@ -25,39 +25,39 @@ class Root extends Container
 			console.log('TRIGGERED');
 		}
 
-		/*var atlasImg = new Sprite('atlas.coin', 600, 400);
-		this.addChild(atlasImg);*/
-
-		/*var atlasImg = new Sprite('atlas.danger', 660, 400);
+		var atlasImg = new ImageSuper('atlas.coin', 600, 400);
 		this.addChild(atlasImg);
 
-		var atlasImg = new Sprite('atlas.super', 660, 300);
+		var atlasImg = new ImageSuper('atlas.danger', 660, 400);
 		this.addChild(atlasImg);
 
-		var atlasImg = new Sprite('atlas.world', 660, 350);
-		this.addChild(atlasImg);*/
+		var atlasImg = new ImageSuper('atlas.super', 660, 300);
+		this.addChild(atlasImg);
 
-		// var tf = new TextField(280, 60, 'bitmapFont', 'Hello á World!');
+		var atlasImg = new ImageSuper('atlas.world', 660, 350);
+		this.addChild(atlasImg);
+
+		var tf = new TextField(280, 60, 'bitmapFont', 'Hello á World!');
 		// tf.hAlign = Align.CENTER;
 		// tf.vAlign = Align.BOTTOM;
-		// tf.align(Align.CENTER, Align.MIDDLE);
-		// tf.position.set(480, 180);
+		tf.align(Align.CENTER, Align.MIDDLE);
+		tf.position.set(480, 180);
 		// tf.appendText(' mais isso');
 		// tf.text = 'ÁÂÃáâãÉéÓÔÕô';
-		// tf.border = true;
-		// this.addChild(tf);
+		tf.border = true;
+		this.addChild(tf);
 
-		var container = new Container();
+		var container = new Sprite();
 		container.name = 'container';
 		this.addChild(container);
 
-		var b1 = new Sprite('imgExample');
+		var b1 = new ImageSuper('imgExample');
 		b1.name = 'b1';
 		b1.position.set(200, 280);
 		b1.scale.set(0.2);
 		container.addChild(b1);
 
-		var b2 = new Sprite('imgExample');
+		var b2 = new ImageSuper('imgExample');
 		b2.name = 'b2';
 		b2.position.set(400, 280);
 		b2.scale.set(0.2);
@@ -66,7 +66,7 @@ class Root extends Container
 		var b3 = new TextField(280, 60, 'bitmapFont', 'Hello Touch');
 		b3.align(Align.CENTER, Align.MIDDLE);
 		b3.border = true;
-		b3.position.set(500, 150);
+		b3.position.set(500, 110);
 		container.addChild(b3);
 
 		var b4 = new Spine('pipoqueiro');
@@ -75,7 +75,6 @@ class Root extends Container
 		container.addChild(b4);
 
 		var objDrag;
-
 		this.addEventListener(TouchEvent.TOUCH, onTouch);
 		function onTouch(e) {
 			var touch = e.getTouch(t2);
@@ -107,5 +106,10 @@ class Root extends Container
 				this.removeEventListener(Event.ENTER_FRAME, loop);
 			}
 		}
+
+		var tile = new TileSprite('atlas.world', 250, 20, 200, 150);
+		this.addChild(tile);
+
+		Script4.juggler.tween(bt, 1.5, { alpha:0.0 });
 	}
 }
