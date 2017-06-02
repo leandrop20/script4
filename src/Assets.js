db608333fef435551bce4933078e4e376e7269e6
Leandro Pinheiro <leo@mentesnotaveis.com.br>
2017-06-02 14:16:12 -0300

    improved assets

--- src/Assets.js
+++ src/Assets.js
@@ -1,15 +1,21 @@
-/**
-* name string
-* path string
-* type string (image, audio, spine, atlas, bitmapfont, particle)
-*/
-const ASSETS = [
-	{ name:'imgExample', path:'assets/images/gameExample.png', type:'image' },
-	{ name:'coin', path:'assets/sounds/coin.mp3', type:'audio' },
-	{ name:'spineboy', path:'assets/animations/spineboy.json', type:'spine'},
-	{ name:'pipoqueiro', path:'assets/animations/pipoqueiro.json', type:'spine'},
-	{ name:'btExample', path:'assets/images/btExample.png', type:'image'},
-	{ name: 'atlas', path:'assets/images/sprites.json', type:'atlas' },
-	{ name: 'bitmapFont', path:'assets/fonts/bitmapfont.xml', type:'bitmapfont' },
-	{ name:'particle', path:'assets/particles/starParticle.png', type:'particle'}
-];
+class Assets
+{
+	/**
+	* name string
+	* path string
+	* type string (image, audio, spine, atlas, bitmapfont, particle)
+	*/
+	static get ASSETS()
+	{
+		return [
+			{ name:'imgExample', path:'assets/images/gameExample.png', type:'image' },
+			{ name:'coin', path:'assets/sounds/coin.mp3', type:'audio' },
+			{ name:'spineboy', path:'assets/animations/spineboy.json', type:'spine'},
+			{ name:'pipoqueiro', path:'assets/animations/pipoqueiro.json', type:'spine'},
+			{ name:'btExample', path:'assets/images/btExample.png', type:'image'},
+			{ name: 'atlas', path:'assets/images/sprites.json', type:'atlas' },
+			{ name: 'bitmapFont', path:'assets/fonts/bitmapfont.xml', type:'bitmapfont' },
+			{ name:'particle', path:'assets/particles/starParticle.png', type:'particle'}
+		];
+	}
+}
