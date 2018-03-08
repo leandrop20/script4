48b35ca50d284f199a8d27c2507117d88bf292f1
Leandro Pinheiro <byleandropinheiro@gmail.com>
2018-03-08 08:24:30 -0300

    import scaleMode

---
+++ src/com/script4/utils/ScaleMode.js
@@ -0,0 +1,8 @@
+export default class ScaleMode {
+
+	static get EXACT_FIT() { return 0; }
+	static get NO_SCALE() { return 1; }
+	static get SHOW_ALL() { return 2; }
+	static get RESIZE() { return 3; }
+	
+}
